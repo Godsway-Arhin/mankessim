69bc00af7f8235559b8ff5fb210e83280f477b6e
Godsway <godswayarhin1@gmail.com>
2026-07-31 15:33:28 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,55 @@
+const CACHE_NAME = "mankessim-v1";
 
+const STATIC_ASSETS = [
+  "/",
+  "/index.html",
+  "/history.html",
+  "/communities.html",
+  "/tourism.html",
+  "/manifest.json",
+
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "/icons/apple-touch-icon.png",
+  "/icons/favicon-32.png",
+  "/icons/favicon-16.png"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).catch(() => caches.match("/index.html"))
+      );
+    })
+  );
+});
